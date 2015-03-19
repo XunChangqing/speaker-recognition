c026649508e9c1b05f67c7b4a1d168c795632fba
xunchangqing <xunchangqing@qq.com>
2015-03-19 16:47:33 +0800

    Feature lpc may contain NaN or infinite, which make gmm unavailable. The fast gmm will not raise error but generate error result, and the sklearn gmm will validate the input and raise a error. We simply only use the feature mfcc to avoid this issue!

--- src/feature/__init__.py
+++ src/feature/__init__.py
@@ -21,9 +21,35 @@ def get_extractor(extract_func, **kwargs):
         return extract_func(*tup, **kwargs)
     return f
 
+#******************************************************
+from scipy import sparse
+def _assert_all_finite(X):
+    """Like assert_all_finite, but only for ndarray."""
+    if (X.dtype.char in np.typecodes['AllFloat'] and not np.isfinite(X.sum())
+            and not np.isfinite(X).all()):
+        raise ValueError("Array contains NaN or infinity.")
+
+
+def assert_all_finite(X):
+    """Throw a ValueError if X contains NaN or infinity.
+
+    Input MUST be an np.ndarray instance or a scipy.sparse matrix."""
+
+    # First try an O(n) time, O(1) space solution for the common case that
+    # there everything is finite; fall back to O(n) space np.isfinite to
+    # prevent false positives from overflow in sum method.
+    _assert_all_finite(X.data if sparse.issparse(X) else X)
+#******************************************************
+
 def mix_feature(tup):
+    #print ('mix feature')
     mfcc = MFCC.extract(tup)
-    lpc = LPC.extract(tup)
+    #assert_all_finite(mfcc)
+    #print ('mfcc ok')
+    #lpc = LPC.extract(tup)
+    #assert_all_finite(lpc)
+    #print ('lpc ok')
     if len(mfcc) == 0:
         print >> sys.stderr, "ERROR.. failed to extract mfcc feature:", len(tup[1])
-    return np.concatenate((mfcc, lpc), axis=1)
+    return mfcc
+    #return np.concatenate((mfcc, lpc), axis=1)
